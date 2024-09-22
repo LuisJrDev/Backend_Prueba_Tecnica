a2e9f1233b3855aa61599699e205366849f41567
Luislemus0109 <Luislemus0109@gmail.com>
2024-09-22 14:40:01 -0500

    Validaciones Categoria

--- Backend_Test/Models/Categoria.cs
+++ Backend_Test/Models/Categoria.cs
@@ -7,6 +7,7 @@ namespace Backend_Test.Models
         public int CategoryId { get; set; }
         [Required(ErrorMessage = "El titulo es obligatorio.")]
         [StringLength(50, ErrorMessage = "El titulo no puede exceder los 50 caracteres.")]
+        [MinLength(5, ErrorMessage = "El contenido debe tener al menos 5 caracteres.")]
         public string Name { get; set; }
 
     }
