8ebfa4b09cdc7d84f8215317a27af6efd6cab540
Luislemus0109 <Luislemus0109@gmail.com>
2024-09-25 15:38:05 -0500

    Bugs Finish

--- Backend_Test/Program.cs
+++ Backend_Test/Program.cs
@@ -2,10 +2,8 @@ using Backend_Test.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -23,7 +21,6 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
